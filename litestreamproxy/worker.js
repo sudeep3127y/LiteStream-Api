2c8320ce9088973cd7af323a4c8d818a0bb33073
sudeep3127y <135289561+sudeep3127y@users.noreply.github.com>
2024-03-06 14:10:47 +0545

    Add files via upload

---
+++ litestreamproxy/worker.js
@@ -0,0 +1,73 @@
+async function handleRequest(request) {
+    const url = new URL(request.url)
+    let apiurl = url.searchParams.get('u')
+
+    if (apiurl === null) {
+        return new Response('Cors Proxy Is Working\n\nUsage: https://proxy.sb543267.workers.dev/?u=https://api3.sb543267.workers.dev')
+    }
+    apiurl = apiurl.replaceAll = ['https://api3.sb543267.workers.dev', 'https://api1.sb543267.workers.dev' , 'https://litestreamapi.sb543267.workers.dev',
+    ]
+    request = new Request(apiurl, request)
+    request.headers.set('Origin', new URL(apiurl).origin)
+
+    const host = url.searchParams.get('host')
+    if (host !== null) {
+        request.headers.set('Host', host)
+    }
+
+    let response = await fetch(request)
+
+    response = new Response(response.body, response)
+    response.headers.set('Access-Control-Allow-Origin', "*")
+    response.headers.append('Vary', 'Origin')
+    return response
+}
+
+function handleOptions(request) {
+    if (
+        request.headers.get('Origin') !== null &&
+        request.headers.get('Access-Control-Request-Method') !== null &&
+        request.headers.get('Access-Control-Request-Headers') !== null
+    ) {
+        return new Response(null, {
+            headers: corsHeaders,
+        })
+    } else {
+        return new Response(null, {
+            headers: {
+                Allow: 'GET, HEAD, POST, OPTIONS',
+            },
+        })
+    }
+}
+
+
+addEventListener('fetch', event => {
+    const request = event.request
+    const url = new URL(request.url)
+    if (request.method === 'OPTIONS') {
+        // Handle CORS preflight requests
+        event.respondWith(handleOptions(request))
+    } else if (
+        request.method === 'GET' ||
+        request.method === 'HEAD' ||
+        request.method === 'POST'
+    ) {
+        // Handle requests to the API server
+        const response = handleRequest(request)
+        event.respondWith(response)
+    } else {
+        event.respondWith(async () => {
+            return new Response(null, {
+                status: 405,
+                statusText: 'Method Not Allowed',
+            })
+        })
+    }
+})
+
+const corsHeaders = {
+    'Access-Control-Allow-Origin': '*',
+    'Access-Control-Allow-Methods': 'GET, HEAD, POST, OPTIONS',
+    'Access-Control-Allow-Headers': 'Content-Type',
+}
